725dbf4c75781f6a6d42967a1cafd4e5f79a48c6
Kevin Yan <kevinyym@gmail.com>
2024-09-28 17:02:40 +0800

    base ui complete

--- hub_detect.py
+++ hub_detect.py
@@ -1,10 +1,24 @@
+# import torch
+#
+# # Model
+# model = torch.hub.load("./", "yolov5s", source="local")
+#
+# # Images
+# img = "./data/images/zidane.jpg"
+#
+# # Inference
+# results = model(img)
+#
+# # Results
+# results.show()
+
 import torch
 
 # Model
-model = torch.hub.load("./", "yolov5s", source="local")
+model = torch.hub.load("./", "custom", path="runs/train/exp2/weights/best.pt", source="local")
 
 # Images
-img = "./data/images/zidane.jpg"
+img = "./datasets/images/train/30.jpg"
 
 # Inference
 results = model(img)
